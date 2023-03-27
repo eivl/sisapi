0701665c182dc491f23d718e38900795cc00f296
Eivind Teig <eivind.teig@gmail.com>
2023-03-27 09:27:55 +0200

    Add new port for testing.

--- app/main.py
+++ app/main.py
@@ -81,4 +81,4 @@ def post_person(person: Person) -> JSONResponse:
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='127.0.0.1')
+    uvicorn.run(app, port=7000, host='127.0.0.1')
